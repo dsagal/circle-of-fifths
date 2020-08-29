b9cad828fb2276255bd8d79bf03f50a5abfcdade
Dmitry S <dsagal+git@gmail.com>
2020-08-28 23:50:22 -0400

    Add scroll-based messages with a description / guide

--- src/log-circle.ts
+++ src/log-circle.ts
@@ -5,6 +5,12 @@ interface Mark {
   label: DomContents;
 }
 
+interface Message {
+  // Start and end refer to the highest value on the ruler, i.e. 1/stretch
+  start: number;
+  msg: DomContents;
+}
+
 // If the string is stretched so that the left-most number is t, then:
 // - the circle has wrapped integral (1 -> t) of L/x, i.e. L*ln(t).
 // - let circleFactor r = R / L (e.g. 0.25)
@@ -31,7 +37,52 @@ const settings = [
   {factor: 10, levelStep: 25},
   {factor: 2, levelStep: 3},
 ];
+
 const {factor, levelStep} = settings[0];
+
+const messages: Message[] = [
+  {start: 1, msg: [
+    dom('p', 'Imagine that the orange line is a rubber band.'),
+    dom('p', 'Scroll to turn the green wheel.')
+  ]},
+  {start: 0.75, msg: [
+    dom('p', 'The wheel is sticky. As it turns, the rubber band sticks to it, ',
+      'remaining as stretched as it was when it touched it.'),
+  ]},
+  {start: 0.55, msg: [
+    dom('p', 'Keep scrolling.'),
+  ]},
+  {start: 0.40, msg: [
+    dom('p', 'Notice how the new ticks on the wheel end up more spread out.'),
+  ]},
+  {start: 0.30, msg: [
+    dom('p', 'Keep scrolling.'),
+  ]},
+  {start: 0.20, msg: [
+    dom('p', 'We show more tick marks as the rubber band keeps on stretching.')
+  ]},
+  {start: 0.13, msg: [
+    dom('p',
+      'The size of the wheel is such that one full turn meets the rubber band when it ',
+      'has stretched by a factor of 10.',
+    ),
+  ]},
+  {start: 0.095, msg: [
+    dom('p', 'For this, the circle\'s circumference is ',
+      dom('i', 'ln(10)'), ' times the initial length of the rubber band.'),
+  ]},
+  {start: 0.075, msg: [
+    dom('p', dom('i', 'ln(10)'), ' is the natural logarithm of 10, about 2.3, ',
+      'the power to which to raise the number ', dom('i', 'e'), ' to get 10.'),
+  ]},
+  {start: 0.055, msg: [
+    dom('p', 'This is the end of the text.'),
+  ]},
+  {start: 0.045, msg: [
+    dom('p', 'Feel free to keep scrolling around.'),
+  ]},
+];
+
 
 const circleFactor = Math.log(factor) / (2*Math.PI);
 const circleRadiusPercent = 100 / (1 + 1 / circleFactor);
@@ -64,29 +115,47 @@ function _buildMarks(densityLevel: number): Mark[] {
 
 function buildPage() {
   history.scrollRestoration = 'manual';
-  return cssPage(
-    cssSection(
-      cssSectionTitle("String Length"),
-      dom.create(buildLogCircle),
-    )
-  );
+  return [
+    cssScrollInner(),
+    dom.create(buildLogCircle),
+  ];
+}
+
+function angleToStretch(angle: number): number {
+  return Math.exp(-angle * circleFactor);
+}
+
+function stretchToAngle(stretch: number): number {
+  return -Math.log(stretch) / circleFactor;
+}
+
+function scrollToAngle(): number {
+  const elem = document.documentElement;
+  return -elem.scrollTop / (elem.offsetWidth * circleRadiusPercent / 100);
+}
+
+function angleToScroll(angle: number): number {
+  const elem = document.documentElement;
+  return -angle * (elem.offsetWidth * circleRadiusPercent / 100);
+}
+
+function setMessagePosition(msg: Message) {
+  const startStretch = 1 / msg.start;
+  return dom.style('top', angleToScroll(stretchToAngle(startStretch)) + 'px');
 }
 
 function buildLogCircle(owner: MultiHolder) {
   const angle = Observable.create(owner, 0);
-  const stretch = Computed.create(owner, angle, (use, a) => Math.exp(-a * circleFactor));
+  const stretch = Computed.create(owner, angle, (use, a) => angleToStretch(a));
   const marksBuilder = buildMarks.bind(null, owner, stretch);
 
-  dom.update(document.body, cssScrollInner());
-  dom.onElem(window, 'scroll', (ev) => {
-    const elem = document.documentElement;
-    angle.set(-elem.scrollTop / (elem.offsetWidth * circleRadiusPercent / 100));
-  });
+  dom.onElem(window, 'scroll', (ev) => angle.set(scrollToAngle()));
 
   return [
+    cssTitle('The Wheel of Logarithms'),
     cssLogCircle(
       cssCirclePart(
-        cssCircleMessage('Scroll down to turn',
+        cssCircleMessage('Scroll page to turn',
           dom.style('opacity', (use) => String(Math.max(0, 1 + use(angle) / Math.PI * 2))),
         ),
         dom.style('transform', (use) => `rotate(${use(angle)}rad)`),
@@ -109,9 +178,18 @@ function buildLogCircle(owner: MultiHolder) {
               dom.style('right', (use) => (value * use(stretch) * 100) + '%')
             ),
           )
+        ),
+      ),
+    ),
+    cssMessages(
+      cssMessagesTop(),
+      cssMessagesBottom(),
+      cssMessagesBody(
+        dom.forEach(messages, (msg) =>
+          cssMessage(setMessagePosition(msg), msg.msg)
         )
       )
-    )
+    ),
   ];
 }
 
@@ -130,19 +208,14 @@ export function frac(numer: number|string, denom?: number|string, ...args: DomEl
     ...args);
 }
 
-const cssPage = styled('div', `
+const cssBody = styled('body', `
   box-sizing: border-box;
-  overflow: hidden;
   font-family: sans-serif;
-  position: fixed;
-  top: 0px;
-  height: 100vh;
-  width: 100vw;
-  padding: 40px;
+  padding: 0px;
+  margin: 0px;
   --overlay-color: rgba(230, 200, 250);
-  display: flex;
-  flex-wrap: wrap;
-  align-items: top;
+  --wheel-color: #00ca00;
+  --text-color: #6745dd;
 
   *, *:before, *:after {
   -webkit-box-sizing: border-box;
@@ -151,27 +224,26 @@ const cssPage = styled('div', `
   }
 `);
 
-const cssSection = styled('div', `
-  flex: auto;
-  &:not(:last-child) {
-    margin-right: 40px;
-  }
-`);
-
-const cssSectionTitle = styled('div', `
-  margin: 20px;
+const cssTitle = styled('h1', `
+  position: fixed;
+  width: 100%;
   text-align: center;
-  font-size: larger;
+  font-size: 20px;
+  margin: 40px;
+  color: var(--text-color);
 `);
 
 const cssLogCircle = styled('div', `
+  position: fixed;
   display: flex;
-  position: relative;
+  width: 100%;
+  top: 60px;
+  padding: 40px;
 `);
 
 const cssScrollInner = styled('div', `
   position: absolute;
-  width: 100%;
+  width: 1px;
   height: 2500%;
 `);
 
@@ -186,7 +258,8 @@ const cssCircleMessage = styled('div', `
 
 const cssCirclePart = styled('div', `
   position: relative;
-  border: 3px solid #00ca00;
+  border: 3px solid orange;
+  box-shadow: inset 0 0 0 3px var(--wheel-color);
   border-radius: 100%;
   width: ${circleRadiusPercent * 2}%;
   margin-right: -${circleRadiusPercent}%;
@@ -205,7 +278,7 @@ const cssCircleCenter = styled('div', `
   position: absolute;
   top: 25%;
   left: 25%;
-  border: 6px dashed #00ca00;
+  border: 6px dashed var(--wheel-color);
   border-radius: 100%;
   width: 50%;
   height: 50%;
@@ -272,4 +345,42 @@ const cssFracPart = styled('div', `
   &-sz6, &-sz7, &-sz8, &-sz9, &-sz10 { font-size: 9px; }
 `);
 
-dom.update(document.body, buildPage());
+const cssMessages = styled('div', `
+  position: absolute;
+  z-index: -1;
+  top: 0px;
+  right: 0px;
+  width: ${100 - 2 * circleRadiusPercent}%;
+  margin: 0 20px;
+`);
+
+const cssMessagesTop = styled('div', `
+  position: fixed;
+  top: 0px;
+  height: 220px;
+  width: 100%;
+  background: linear-gradient(white, white 80%, transparent);
+  z-index: 1;
+`);
+
+const cssMessagesBottom = styled(cssMessagesTop, `
+  bottom: 0px;
+  top: unset;
+  height: calc(100% - 450px);
+  background: linear-gradient(transparent, white 20%, white);
+`);
+
+const cssMessagesBody = styled('div', `
+  position: relative;
+  max-width: 400px;
+  margin: 220px auto auto 100px;
+  color: var(--text-color);
+`);
+
+const cssMessage = styled('div', `
+  position: absolute;
+  font-size: 18px;
+  line-height: 1.5;
+`);
+
+dom.update(document.body, cssBody.cls(''), buildPage());
